97aee63bbeeb8254d13fe2f7ca048ee982245c89
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-01-24 22:20:00 +0530

    Add Array problem solution

--- 06-array/08-Max Chunks To Make Sorted.java
+++ 06-array/08-Max Chunks To Make Sorted.java
@@ -1,6 +1,7 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
         // Time complexity is O(N) and space complexity is O(1).
+        // Algorithm used: Greedy.
         int count = 0;
         int maxSeen = 0;
         for (int i = 0; i < arr.length; i++) {
