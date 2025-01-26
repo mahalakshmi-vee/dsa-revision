beaf6d139cca2fab4d800c8c4c2ff3e9780f080f
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-01-26 12:33:12 +0530

    Add Array problem solution

---
+++ 06-array/29-Maximum Gap.java
@@ -0,0 +1,14 @@
+// class Solution {
+    // public int maximumGap(int[] nums) {
+        // Submitted brute force approach:
+        // Time complexity is O(NLogN)
+        // Space complexity is O(1)
+        // Arrays.sort(nums);
+        // int answer = 0;
+        // int n = nums.length;
+        // for (int i = 0; i < n - 1; i++) {
+        //     answer = Math.max(answer, nums[i + 1] - nums[i]);
+        // }
+        // return answer;
+    // }
+// }
