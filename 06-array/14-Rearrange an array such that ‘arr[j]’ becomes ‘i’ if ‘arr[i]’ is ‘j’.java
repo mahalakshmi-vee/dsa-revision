0a30ba7bf261621633039282539e316ac51c9b91
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-01-26 16:08:48 +0530

    Add Array problem solution

--- 06-array/14-Rearrange an array such that ‘arr[j]’ becomes ‘i’ if ‘arr[i]’ is ‘j’.java
+++ 06-array/14-Rearrange an array such that ‘arr[j]’ becomes ‘i’ if ‘arr[i]’ is ‘j’.java
@@ -27,3 +27,34 @@
 //         }
 //     }
 // }
+
+
+// Optimized approach: Using modulo arithmetic operations.
+// Time complexity is O(N)
+// Space Complexity is O(1)
+class Main {
+    public static void main(String[] args) {
+        int[] input = {2, 0, 1, 4, 5, 3};
+        // arr[1] = 0;
+        // arr[3] = 1;
+        // arr[0] = 2;
+        // arr[2] = 3
+        // output array should have {2, 0, 3, 1}
+
+        int n = input.length;
+
+        for (int i = 0; i < n; i++) {
+            input[input[i] % n] += (i * n);
+        }   
+         
+        for (int i = 0; i < n; i++) {
+            input[i] = input[i] / n;
+        }
+         
+        System.out.println("Printing the results:");
+        for (int i = 0; i < n; i++) {
+            System.out.println(input[i]);
+        }
+    }
+}
+
