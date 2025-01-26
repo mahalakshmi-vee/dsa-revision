9b06165490cc625d4fe56a821bfac91998bfc4dc
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-01-26 17:02:57 +0530

    Add Array problem solution

--- 06-array/29-Maximum Gap.java
+++ 06-array/29-Maximum Gap.java
@@ -11,4 +11,17 @@
         // }
         // return answer;
     // }
-// }
+// // }
+// Example:
+// input array = {21, 9, 25, 3, 37, 43, 49, 29}
+
+// Sorted input array = {3, 9, 21, 25, 29, 37, 43, 49} 
+// Important point: WE ARE NOT ALLOWED TO DO THE SORTING OPERATION.
+// 3-9 Gap is 6
+// 21-9 Gap is 12
+// 25-21 Gap is 4
+// 29-25 Gap is 4
+// 37-29 Gap is 8
+// 43-37 Gap is 6
+// 49-43 Gap is 6
+// Maximum gap among all of the above gap is 12. Hence answer is 12.
