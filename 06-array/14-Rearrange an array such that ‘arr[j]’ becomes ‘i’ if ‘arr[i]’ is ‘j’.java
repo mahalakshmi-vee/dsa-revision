e3b54d397cf6301c533fdb180bc860dd4a376d53
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-01-26 16:03:39 +0530

    Add Array problem solution

---
+++ 06-array/14-Rearrange an array such that ‘arr[j]’ becomes ‘i’ if ‘arr[i]’ is ‘j’.java
@@ -0,0 +1,29 @@
+
+// Brute force approach:
+// Time complexity is O(N)
+// Space Complexity is O(N)
+// class Main {
+//     public static void main(String[] args) {
+//         int[] input = {2, 0, 1, 4, 5, 3};
+        // arr[1] = 0;
+        // arr[3] = 1;
+        // arr[0] = 2;
+        // arr[2] = 3
+        // output array should have {2, 0, 3, 1}
+
+//         int n = input.length;
+//         int[] resultantArray = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             resultantArray[input[i]] = i;
+//         }
+        
+//         for (int i = 0; i < n; i++) {
+//             input[i] = resultantArray[i];
+//         }
+        
+//         System.out.println("Printing the results:");
+//         for (int i = 0; i < n; i++) {
+//             System.out.println(input[i]);
+//         }
+//     }
+// }
