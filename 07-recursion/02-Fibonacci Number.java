b917ea123ab6118df62c5184176c58269ee50f2c
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-01-27 21:43:54 +0530

    Update 02-Fibonacci Number.java

--- 07-recursion/02-Fibonacci Number.java
+++ 07-recursion/02-Fibonacci Number.java
@@ -1,6 +1,8 @@
 class Solution {
     public int fib(int n) {
+        // Base Case.
         if (n <= 1) return n;
+        // Recurrence Relation.
         return fib(n - 1) + fib(n - 2);
     }
 }
