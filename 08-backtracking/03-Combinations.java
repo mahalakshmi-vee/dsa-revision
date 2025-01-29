066670299a10cb8b01385338a62f0ec56ea50727
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-01-29 11:04:27 +0530

    Add Backtracking problem solution

---
+++ 08-backtracking/03-Combinations.java
@@ -0,0 +1,26 @@
+class Solution {
+    // Time Complexity: O(n c with k) - Binomial Coefficient
+    // Space Complexity: O(n c with k)
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> listAnswer = new ArrayList<>();
+        List<Integer> listNum = new ArrayList<>();
+        for (int i = 1; i <= n; i++) listNum.add(i);
+        find (listNum, listAnswer, 0, new ArrayList<>(), k);
+        return listAnswer;
+    }
+    private void find (List<Integer> listNum, List<List<Integer>> listAnswer, int index, List<Integer> listSubAnswer, int k) {
+        if (listSubAnswer.size() == k) {
+            listAnswer.add(new ArrayList<>(listSubAnswer));
+            return;
+        }
+        for (int i = index; i < listNum.size(); i++) {
+            listSubAnswer.add(listNum.get(i));
+            find (listNum, listAnswer, i + 1, listSubAnswer, k);
+            listSubAnswer.remove(listSubAnswer.size() - 1);
+        }
+    }
+}
+// Time Complexity Analysis:
+// Binomial Coefficient is denoted as "n choose k" represents the number of ways to choose k elements from the set of n elements.
+// We are generating all the possible valid combinations of k elements from the n elements. Hence the time complexity of this algorithm 
+// is binomial coeeficient.
