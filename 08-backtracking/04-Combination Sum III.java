7791bd1b7ff55b9bec0e1151a00845bb3137b6bd
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-01-31 17:48:18 +0530

    Add Backtracking problem solution

--- 08-backtracking/04-Combination Sum III.java
+++ 08-backtracking/04-Combination Sum III.java
@@ -3,10 +3,10 @@ class Solution {
         List<List<Integer>> listAnswer = new ArrayList<>();
         List<Integer> listNumber = new ArrayList<>();
         for (int i = 1; i <= 9; i++) listNumber.add(i);
-        find (listNumber, listAnswer, new ArrayList<>(), k, n, 0);
+        backtrack (listNumber, listAnswer, new ArrayList<>(), k, n, 0);
         return listAnswer;
     }
-    private void find (List<Integer> listNumber, List<List<Integer>> listAnswer, List<Integer> listSubAnswer, int k, int n, int index) {
+    private void backtrack (List<Integer> listNumber, List<List<Integer>> listAnswer, List<Integer> listSubAnswer, int k, int n, int index) {
         if (listSubAnswer.size() == k && n == 0) {
             listAnswer.add(new ArrayList<>(listSubAnswer));
             return;
@@ -16,10 +16,10 @@ class Solution {
         }
 
         listSubAnswer.add(listNumber.get(index));
-        find (listNumber, listAnswer, listSubAnswer, k, n - listNumber.get(index), index + 1);
+        backtrack (listNumber, listAnswer, listSubAnswer, k, n - listNumber.get(index), index + 1);
         
         listSubAnswer.remove(listSubAnswer.size() - 1);
-        find (listNumber, listAnswer, listSubAnswer, k, n, index + 1);
+        backtrack (listNumber, listAnswer, listSubAnswer, k, n, index + 1);
     }
 }
 
