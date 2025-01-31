f50e36bf30be9382907a57924c12cefce3e6d8e5
Mahalakshmi Veeraraj <mahalakshmi.veeraraj.scaler@gmail.com>
2025-01-31 11:45:25 +0530

    Add Recursion problem solution

---
+++ 08-backtracking/06-Combination Sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        List<List<Integer>> listAnswer = new ArrayList<>();
+        find (listAnswer, candidates, target, 0, new ArrayList<>());
+        return listAnswer;
+    }
+    private void find (List<List<Integer>> listAnswer, int[] candidates, int target, int index, List<Integer> listSubAnswer) {
+        if (target == 0) {
+            listAnswer.add(new ArrayList<>(listSubAnswer));
+            return;
+        }
+        else if (index >= candidates.length || candidates[index] > target) {
+            return;
+        }
+
+        // include the element
+        listSubAnswer.add(candidates[index]);
+        find (listAnswer, candidates, target - candidates[index], index, listSubAnswer);
+
+        // exclude the element
+        listSubAnswer.remove(listSubAnswer.size() - 1);
+        find (listAnswer, candidates, target, index + 1, listSubAnswer);
+    }
+}
+
+// Time Complexity: (2 ^ T)
+// Space Complexity: O(T)
